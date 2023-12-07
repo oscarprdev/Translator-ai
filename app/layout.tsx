d559cf7c4efa136351ec7041948367743e090149
oscarprdev <vlcoscar8@gmail.com>
2023-12-07 08:06:32 +0100

    feat: add edge runtime on layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,8 @@ export const metadata: Metadata = {
 	description: 'Language translator powered by AI',
 };
 
+export const runtime = 'edge';
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang='en'>
