2b7ab7ef3c85b7fca4edf7601a6f70c6283ff503
oscarprdev <vlcoscar8@gmail.com>
2023-12-07 08:22:17 +0100

    feat: remove edge runtime

--- app/components/translate-card-server/translate-card-server.tsx
+++ app/components/translate-card-server/translate-card-server.tsx
@@ -3,8 +3,6 @@
 import { translateInfoAction } from '../../actions/translate-input/translate-input.action';
 import WordCard from '../word-card/word-card';
 
-export const runtime = 'edge';
-
 interface TranslateCardServerProps {
 	input: string;
 	langInput: string;
